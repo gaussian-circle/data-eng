35dd8e02a32896f0a29141cba00d98cc9d384bf4
gaussian-circle <mcptesting3@gmail.com>
2026-01-19 12:21:19 +0000

    provision vm instance on gcp

--- mod_1/learn-terraform-gcp/main.tf
+++ mod_1/learn-terraform-gcp/main.tf
@@ -13,6 +13,40 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
+# This data source gets a temporary token for the service account
+data "google_service_account_access_token" "default" {
+  provider               = google
+  target_service_account = "terraform-manager@project-791f23ef-cd77-4fab-a72.iam.gserviceaccount.com"
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "3600s"
+}
+
+# This second provider block uses that temporary token
+provider "google" {
+  alias        = "impersonated"
+  access_token = data.google_service_account_access_token.default.access_token
+  project      = "project-791f23ef-cd77-4fab-a72"
+  region       = "us-central1"
+}
+
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
